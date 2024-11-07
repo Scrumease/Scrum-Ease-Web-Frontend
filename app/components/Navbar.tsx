cd68eb4754167023d6adbd800c67152267f5c46f
victor medeiros <victorehrich@gmail.com>
2024-11-06 22:20:15 -0300

    style: change images

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,13 @@ import UserIcon from "./UserIcon";
 import NavbarItems from "./navbar/NavbarItems";
 import { getUserInfoFromToken } from "../utils/token";
 import Link from "next/link";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Navbar = () => {
   const userInfo = getUserInfoFromToken();
+  const { theme } = useContext(ThemeContext);
 
   return (
     <div className="navbar bg-base-100">
@@ -17,15 +21,31 @@ const Navbar = () => {
         </label>
       </div>
       <div className="flex-1">
-        <Link href={"/"} className="btn btn-ghost normal-case text-xl">Dashboard</Link>
+        <Link href={"/"} className="btn btn-ghost normal-case text-xl">
+          {theme === "light" ? (
+            <Image
+              src="/logo/logo-gray-no-background.svg"
+              width={120}
+              height={50}
+              alt="ScrumEase"
+            />
+          ) : (
+            <Image
+              src="/logo/logo-colored-no-background.svg"
+              width={120}
+              height={50}
+              alt="ScrumEase"
+            />
+          )}
+        </Link>
       </div>
       <div className="flex-none gap-2">
         <ThemeController />
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button">
-            <UserIcon decodedToken={userInfo}/>
+            <UserIcon decodedToken={userInfo} />
           </div>
-          <NavbarItems decodedToken={userInfo}/>
+          <NavbarItems decodedToken={userInfo} />
         </div>
       </div>
     </div>
@@ -33,5 +53,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
