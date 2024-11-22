9afa9b2422d38053afee8afc709b6ad9f0f26f5e
victor medeiros <victorehrich@gmail.com>
2024-11-22 00:02:26 -0300

    feat: update role

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,50 +1,56 @@
-import { ReactNode, useEffect, useRef } from 'react';
+import { ReactNode, useEffect, useRef } from "react";
 
 interface ModalProps {
   title?: string;
   children: ReactNode;
   isOpen: boolean;
-  id: string
+  id: string;
   onClose: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ title, children, isOpen, onClose, id, ...dialogProps  }) => {
+const Modal: React.FC<ModalProps> = ({
+  title,
+  children,
+  isOpen,
+  onClose,
+  id,
+  ...dialogProps
+}) => {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
     const dialog = dialogRef.current;
     if (isOpen && dialog) {
       dialog.showModal();
-    } else if (
-      !isOpen &&
-      dialog
-    ) {
+    } else if (!isOpen && dialog) {
       dialog.close();
-    } 
+    }
   }, [isOpen, id]);
 
   return (
     <>
-    {isOpen && (
+      {isOpen && (
         <dialog
-        ref={dialogRef}
-        {...dialogProps}
-        id={id}
-        className="modal"
-        onClose={onClose}
-      >
-        <div className="modal-box">
-          {title && <h3 className="font-bold text-lg">{title}</h3>}
-          <div className="py-4">{children}</div>
-        </div>
-        <form method="dialog" className="modal-backdrop w-full h-full" onClick={onClose}>
-          <button>close</button>
-        </form>
-      </dialog>
-    )}
-
+          ref={dialogRef}
+          {...dialogProps}
+          id={id}
+          className="modal"
+          onClose={onClose}
+        >
+          <div className="modal-box">
+            {title && <h3 className="font-bold text-lg">{title}</h3>}
+            <div className="py-4">{children}</div>
+          </div>
+          <form
+            method="dialog"
+            className="modal-backdrop w-full h-full"
+            onClick={onClose}
+          >
+            <button>close</button>
+          </form>
+        </dialog>
+      )}
     </>
-
   );
 };
 
